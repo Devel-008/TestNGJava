dfdc99b56f5a68a384736659ab26decd92927164
ISHA SETHIA <isha.sethia@confluxsys.com>
2022-07-08 14:59:21 +0530

    Initial commit

---
+++ src/TestNG.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+
+public class TestNG {
+    @Test
+    void test()
+    {
+        System.out.println("INSIDE TEST!");
+    }
+    @Test
+    void test1()
+    {
+        System.out.println("INSIDE TEST1!!");
+    }
+    @Test
+    void test2()
+    {
+        System.out.println("INSIDE TEST2!!!");
+    }
+}
